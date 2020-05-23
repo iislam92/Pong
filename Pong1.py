606beb844a619e9a5d20261f14362c821aa6a6ac
Intisar Islam <65798883+iislam92@users.noreply.github.com>
2020-05-23 13:10:58 -0400

    Update Pong1.py
    Added code to allow for ball movement and border checking

--- Pong1.py
+++ Pong1.py
@@ -1,6 +1,6 @@
 # Simple Pong in Python 3 for Beginners
 
-import turtle
+import turtle # Module to add graphics
 
 wn = turtle.Screen()
 wn.title("Pong by Intisar")
@@ -15,24 +15,27 @@ paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
-paddle_a.goto(-350, 0)
+paddle_a.goto(-375, 0)
 
 # Paddle B
-paddle_b = turtle.Turtle()
-paddle_b.speed(0)
+paddle_b = turtle.Turtle() # turtle - module name Turtle - class name
+paddle_b.speed(0) # Sets the animation to maximum speed
 paddle_b.shape("square")
 paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
-paddle_b.goto(350, 0)
+paddle_b.goto(375, 0)
 
 # Ball
-ball= turtle.Turtle() 
-ball.speed(0) 
+ball= turtle.Turtle() # turtle - module name Turtle - class name
+ball.speed(0) # Sets the animation to maximum speed
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2.5 # Everytime our ball moves, it will move by this amount of pixels
+ball.dy = -2.5
+
 
 # Function to allow for the two paddles to move up and down
 def paddle_a_up():
@@ -66,4 +69,23 @@ wn.onkeypress(paddle_b_down, "Down") # When down arrow is pressed, paddle_b_up f
 while True:
     wn.update()
 
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
 
+    # Border Checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1 # Reverses the direction of the ball when it hits the top border
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1  # Reverses the direction of the ball when it hits the bottom border
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0) # If ball goes off to the side, ball moves back to center
+        ball.dx *= -1 # Reverses direction once ball moves back to center
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1 
